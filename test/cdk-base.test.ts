a195ce732ce7e87d9bf20dc09f41ab16c11fc8a0
Gustavo S. Rodrigues <gustavo.rodrigues@maxmilhas.com.br>
2022-03-09 06:57:12 -0300

    Initial commit
    It’s the first commit of the project that was started with CDK init command.
    
     ``cdk init app --language typescript``
    
     After then, Ichangede some configs and path structures.
    
    ref: https://docs.aws.amazon.com/cdk/v2/guide/work-with-cdk-typescript.html

---
+++ test/cdk-base.test.ts
@@ -0,0 +1,16 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkBaseStack from '../infraestructure/cdk-base-stack';
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-base-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkBaseStack.CdkBaseStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
