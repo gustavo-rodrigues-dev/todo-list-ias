967f1e9e9b275db5be59ce5219536c89007e1503
Gustavo S. Rodrigues <gustavo.rodrigues@maxmilhas.com.br>
2022-03-29 07:18:15 -0300

    feat: Add API Permissions to edit Bucket
    Add a role to put and read objects into the Todo S3 Bucket.

--- infrastructure/api-lambda.ts
+++ infrastructure/api-lambda.ts
@@ -3,16 +3,40 @@ import { DockerImageFunction, DockerImageCode } from 'aws-cdk-lib/aws-lambda';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import path from 'path';
 import { StackResource } from './stack-resource';
-import { ToDoApiGateway, ToDoLambdaApi } from './resources';
+import { ToDoApiGateway, ToDoBucket, ToDoLambdaApi } from './resources';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class ApiFunctionStack {
   constructor(stack: StackResource) {
+    const bucket: Bucket | undefined = stack.resources.get(
+      ToDoBucket,
+    ) as Bucket;
     const apiFunction = new DockerImageFunction(stack, 'ApiLambda', {
       code: DockerImageCode.fromImageAsset(path.join(__dirname, '..', 'api'), {
         cmd: ['index.handleHello'],
         entrypoint: ['/lambda-entrypoint.sh'],
       }),
     });
+
+    if (!bucket) {
+      throw new Error(`Bucket ${ToDoBucket.toString()} is no available`);
+    }
+
+    const bucketContainerPermissions = new PolicyStatement({
+      effect: Effect.ALLOW,
+      resources: [bucket.bucketArn],
+      actions: ['S3:ListBucket'],
+    });
+
+    const bucketPermissions = new PolicyStatement({
+      effect: Effect.ALLOW,
+      resources: [`${bucket.bucketArn}/*`],
+      actions: ['S3:GetObject', 'S3:PutObject'],
+    });
+
+    apiFunction.addToRolePolicy(bucketContainerPermissions);
+    apiFunction.addToRolePolicy(bucketPermissions);
 
     const apiGateway = new LambdaRestApi(stack, 'ToDoApi', {
       handler: apiFunction,
