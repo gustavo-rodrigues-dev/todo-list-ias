faf19de73ea0d97639d5193c0d71ecd4b379fec6
Gustavo S. Rodrigues <gustavo.rodrigues@maxmilhas.com.br>
2022-04-03 12:07:51 -0300

    refactor: re-lint project
    update linters config to all project

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,19 +1,22 @@
 import {
-    APIGatewayProxyEventV2,
-    Context,
-    APIGatewayProxyResultV2,
+  APIGatewayProxyEventV2,
+  Context,
+  APIGatewayProxyResultV2,
 } from 'aws-lambda';
 import { bootstrap } from './main';
 import { AppService } from './app.service';
 
 // eslint-disable-next-line unused-imports/no-unused-vars-ts
-export const handleHello = async (event: APIGatewayProxyEventV2, context: Context): Promise<APIGatewayProxyResultV2> => {
-    const app = await bootstrap();
-    const service = app.get(AppService);
-    const result: APIGatewayProxyResultV2 = {
-        statusCode: 200,
-        body: service.getHello(),
-    };
+export const handleHello = async (
+  event: APIGatewayProxyEventV2,
+  context: Context,
+): Promise<APIGatewayProxyResultV2> => {
+  const app = await bootstrap();
+  const service = app.get(AppService);
+  const result: APIGatewayProxyResultV2 = {
+    statusCode: 200,
+    body: service.getHello(),
+  };
 
-    return result;
+  return result;
 };
--- api/src/main.ts
+++ api/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplicationContext } from '@nestjs/common';
 
-export async function bootstrap(): Promise<INestApplicationContext>  {
+export async function bootstrap(): Promise<INestApplicationContext> {
   const app = await NestFactory.createApplicationContext(AppModule);
   return app;
 }
